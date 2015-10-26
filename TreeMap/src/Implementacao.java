aa3421dad89803ea7f93ee39ce0e64529030fa02
Drei <Drei@Drei-PC>
2015-10-26 16:36:35 -0200

    Adicionando comentarios

--- TreeMap/src/Implementacao.java
+++ TreeMap/src/Implementacao.java
@@ -2,26 +2,33 @@ import java.util.TreeMap;
 
 public class Implementacao {
 	
+	//Instancia de um objeto do tipo treemap, neste contexto a chave dessa treempa é uma string e o valor é um inteiro,
+	//porem esses tipos podem ser invertidos ou alterados para satisfazer as necessidade da aplicação.
 	TreeMap<String, Integer> nomes = new TreeMap<String, Integer>();
 	
+	//Metodo que adiciona uma chave e um valor na treemap.
 	public void addKV(String key, int value){
 		nomes.put(key,value);
 	}
 	
+	//Metodo que remove uma unica chave.
 	public void removeK(String key){
 		nomes.remove(key);
 	}
 	
+	//Metodo que busca e imprime uma unica chave.
 	public void buscaUmaK(String key){
 		System.out.println(key  +" :: "+ nomes.get(key));
 	}
 	
+	//Metodo que imprime todas as chaves e valores da treemap.
 	public void buscaTodasK(){
 		for(String key: nomes.keySet()){
             System.out.println(key  +" :: "+ nomes.get(key));
         }
 	}
 	
+	//Metodo que limpa todas das chaves e valores da treemap.
 	public void limpaTodoMap(){
 		nomes.clear();
 	}
--- TreeMap/src/Testadora.java
+++ TreeMap/src/Testadora.java
@@ -3,21 +3,39 @@ public class Testadora {
 	
 	
 	public static void main(String[] args) {
+		//Intancia de um objeto do tipo Implementacao, que sera responsavel por chamar os metodos 
+		// da classe implementacao.
 		Implementacao teste = new Implementacao();
 		
+		
+		//Inicio dos testes
+		
+		//Adicionando chaves e valores
 		teste.addKV("Joao", 80);
 		teste.addKV("Maria", 2);
 		teste.addKV("Andre", 7);
 		
+		//**************************************************************************************
+		//Buscando todas as chaves da treemap.
 		teste.buscaTodasK();
 		
+		//***************************************************************************************
+		//Buscando uma unica chave
 		//teste.buscaUmaK("Joao");
 		
+		
+		//****************************************************************************************
+		//Removendo uma unica chave
 		teste.removeK("Joao");
 		
+		
+		//*****************************************************************************************
+		//Limpando toda a treemap
 		//teste.limpaTodoMap();
 		
 		
+		//*****************************************************************************************
+		teste.buscaTodasK();
 	}
 
 }
